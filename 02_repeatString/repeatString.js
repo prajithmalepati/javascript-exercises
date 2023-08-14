4497ff003b37ca5f4dd9c8b1743d61d59e895a0f
Prajith <79435415+prajithmalepati@users.noreply.github.com>
2023-08-14 10:57:31 -0400

    02_done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    if(times<0){
+        return "ERROR"
+    }
+    let newWord="";
+    for(let i =0;i<times;i++){
+        newWord+=word;
+    }
+    return newWord;
 };
 
 // Do not edit below this line
