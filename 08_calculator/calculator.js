9a192d0ea85b21737e5fb9f5623daaae7ac96ea9
Prajith <79435415+prajithmalepati@users.noreply.github.com>
2023-08-22 12:38:25 -0400

    08 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,34 @@
-const add = function() {
+const add = function(a,b) {
+	return a+b;
+};
+
+const subtract = function(a,b) {
+	return a-b;
+};
+
+const sum = function(array) {
+	return array.reduce((total, current) =>total+current,0)
+};
+
+const multiply = function(array) {
+  return array.reduce((product, current)=>product*current
+  )
+};
+
+const power = function(a,b) {
+  return Math.pow(a,b);
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(n) {
+  let product = 1;
+  if(n===0){
+    return 1
+  }
+  for (let i =1;i<=n;i++){
+    product*=i;
+  }
+  return product;
 	
 };
 
