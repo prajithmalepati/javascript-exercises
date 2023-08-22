38296616c4cfc2cccc63d78a5dcaa8cf3303363e
Prajith <79435415+prajithmalepati@users.noreply.github.com>
2023-08-22 14:18:28 -0400

    09 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
+const palindromes = function (line) {
+    let newLine = line.toLowerCase().replace(/[^a-z0-9]/g, "");
+    let reversedLine = newLine.split("").reverse().join("");
+    return newLine==reversedLine;
+  };
 
-};
 
 // Do not edit below this line
 module.exports = palindromes;
