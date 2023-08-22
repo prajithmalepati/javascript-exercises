1825acdb849d20cb9aadfc618e45d28daf96f060
Prajith <79435415+prajithmalepati@users.noreply.github.com>
2023-08-22 15:14:21 -0400

    10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(int) {
+    if(int<0){
+        return "OOPS";
+    }
+    const sequence = [];
+    sequence[0] = 0;
+    sequence[1]=1;
+    for(let i =2;i<=int;i++){
+        sequence[i]=sequence[i-1]+sequence[i-2];
+    }
+    return sequence[int];
 
 };
 
